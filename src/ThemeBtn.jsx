3848d530f402c6329cc8012b2c598c48cebd7439
aitzaza032-aitzaz <aitzaza032@gmail.com>
2026-08-27 20:44:23 -0700

    Update ThemeBtn.jsx

--- src/ThemeBtn.jsx
+++ src/ThemeBtn.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import  useTheme from '../contexts/theme.js'
+import useTheme from './theme.js'
 function ThemeBtn() {
 
     const { themeMode , lightTheme , darkTheme } = useTheme()
